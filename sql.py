61dd89c01ea3a6d425612d9258afda91390b46d1
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-08-30 06:41:04 +0330

    Update sql.py

--- sql.py
+++ sql.py
@@ -32,3 +32,292 @@ def check_changes(url):
 print("                               ")     
 url = input(" < EnTeR ThE SitE UrL ~> ")
 check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
+import requests
+
+def check_changes(url):
+    original_url = url
+    modified_url = url + "'"
+    
+    original_response = requests.get(original_url)
+    modified_response = requests.get(modified_url)
+    
+    if original_response.text == modified_response.text:
+        print(" !SITE DONT HAVE SQLI BUG! ")
+    else:
+        print(" !SITE HAVE SQLI BUG! ")
+        
+print("                               ")     
+url = input(" < EnTeR ThE SitE UrL ~> ")
+check_changes(url)
