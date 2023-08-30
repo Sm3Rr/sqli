0df1bf77210fa800d679a5ac18a166b92ff02448
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-08-30 06:29:14 +0330

    Update sql.py

--- sql.py
+++ sql.py
@@ -1,20 +1,20 @@
 print ('''
-	           //////////////
-	           | SqLi  BuG |/
-	           |-----------|/
-	           |  checker  |/ 
-	           |___________|/
->--------------------------------------------->
+	          //////////////
+	          | SqLi  BuG |/
+	          |-----------|/
+	          |  checker  |/ 
+	          |___________|/
+>------------------------------------------------->
 Version 1.0.0 (sqli bug checker)
                         C0d3d by SecurityFucker
-├──────────────────────────────────────┤
-│                How To Use:                  │
-│          [1] First Enter Target Url         │
-│          [2] Make sure it have parameter    │
-│          [3] Now u Can Find Out !           │
-├──────────────────────────────────────┤
-│ Link: https://github.com/Sm3Rr/sqli         │
-└──────────────────────────────────────┘''')
+├─────────────────────────────────────────┤
+│                How To Use:              │
+│         [1] First Enter Target Url      │
+│         [2] Make sure it have parameter │
+│         [3] Now u Can Find Out !        │
+├─────────────────────────────────────────┤
+│ Link: https://github.com/Sm3Rr/sqli        │
+└─────────────────────────────────────┘''')
 import requests
 
 def check_changes(url):
